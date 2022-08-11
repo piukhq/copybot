14f749640b090ddc2e8af2f8bc1bd4a708213094
Thenuja Viknarajah <thenuja_1@outlook.com>
2022-08-11 12:57:32 +0100

    add application name to db

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     postgres_host: str = "postgresql://postgres@localhost:5432/{}"
     postgres_db: str = "postgres"
+    postgres_connect_args: dict = {"application_name", "copybot"}
     amqp_url: str = "amqp://guest:guest@localhost:5672/"
     datetime_format: str = "%Y-%m-%d %H:%M:%S.%f"
     debug: bool = False
